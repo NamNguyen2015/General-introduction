ffbb10ba4ae7c186210c748fb8debb3103cabd77
NamNguyen2015 <111955400+NamNguyen2015@users.noreply.github.com>
2022-09-13 10:22:20 +0200

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -39,7 +39,7 @@ st.markdown("**Plotly Dash vs Streamlit — Which is the best?**")
 st.markdown("[History of GitHub stars for both Plotly Dash and Streamlit](https://towardsdatascience.com/plotly-dash-vs-streamlit-which-is-the-best-library-for-building-data-dashboard-web-apps-97d7c98b938c)")
 image = "https://upload.wikimedia.org/wikipedia/commons/7/77/Streamlit-logo-primary-colormark-darktext.png"
 
-st.image(image, caption='The fastest way to build and share data apps', width=300)
+st.image(image, caption='The fastest way to build and share data apps', width=500)
 st.write('Streamlit is an open source app framework in Python. It helps us create web apps for data science and machine learning in a short time. It is compatible with major Python libraries such as scikit-learn, Keras, PyTorch, SymPy, NumPy, pandas, Matplotlib etc.')
 
 st.write('**************************************')
